66872b6240821c11ba7d14f293f217dd8b578d38
albreaiki <al.b.ryk.ys20.3@gmail.com>
2025-06-18 23:22:13 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot: {
         tokens: [
-          "YOUR_BOT_TOKEN_HERE",              // Main bot token - Replace with your actual token
+          "MTM4NDk1Mjg2OTQ3OTc4MDQ0Mw.GIfDaC.2eIx3I3vnAQSKzOgDN6HjiUjiGJo3jXuMd6Es8",              // Main bot token - Replace with your actual token
         //  "YOUR_SECOND_BOT_TOKEN_HERE",      // 2 bot token - Uncomment and replace to use
         //  "YOUR_THIRD_BOT_TOKEN_HERE",       // 3 bot token - Uncomment and replace to use
         //  "YOUR_FOURTH_BOT_TOKEN_HERE",      // 4 bot token - Uncomment and replace to use
@@ -17,9 +17,9 @@ module.exports = {
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1374788327714197615',         // ايدي السيرفر
+        broadcastRoleId: '1375547458549059644', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1384970212046143659' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
